bf54251368018ee971153db8060af43886a144df
Zhang Yaxin <Valses@users.noreply.github.com>
2016-09-24 16:13:25 +0800

    Update spn.c

--- spn.c
+++ spn.c
@@ -1,41 +1,55 @@
-/** \brief
- *
- * \param
- * \param
- * \return
- *
+/** 任务一：原始SPN加密
+ * 描述：
+ * 分组长度为4，轮数为4
+ * 加解密分别100000次，记录总时间
  */
 
 #include <stdio.h>
 #include <stdlib.h>
-#define Nr 4       //��������
-#define BIT 16      //����λ������p�г���
-#define L 4         //ÿ��s�е�����bit��
-#define LS 16       //s�г���
+#include <time.h>
+
+#define Nr 4        //加密轮数
+#define BIT 16      //明文位数，即p盒长度
+#define L 4         //每个s盒的输入bit数
+#define LS 16       //s盒长度
 
 typedef unsigned short uint16_t;
 typedef unsigned int uint32_t;
-typedef uint16_t spncode;
-uint32_t K = 0x3A94D63F;     //ԭʼ��Կ
+typedef uint16_t uint16_t;
+uint32_t K = 0x3A94D63F;     //原始密钥
 uint16_t s[LS] = {14,4,13,1,2,15,11,8,3,10,6,12,5,9,0,7};
-uint16_t p[BIT] = {1,5,9,13,2,6,10,14,3,7,11,15,4,8,12,16};
-//s�к�p�еĶ���
+uint16_t s_in[LS] =  {14,3,4,8,1,12,10,15,7,13,9,6,11,2,0,5};
+//s盒与s逆的定义
 
-spncode sbox(spncode c){
-    spncode s_chiper = 0;
-    spncode mask = 0xf;
-    spncode m = 0;         //��¼s�к������м�ֵ
+uint16_t sbox(uint16_t c){
+	//加密s盒
+    uint16_t s_cipher = 0;
+    uint16_t mask = 0xf;
+    uint16_t m = 0;         //记录s盒函数的中间值
     int i = 1;
     for(i = 1;i <= (BIT/L);i++){
         m = (c >> (BIT - (i*4))) & mask;
-        s_chiper = (s[m] << (BIT - (i*4))) | s_chiper;
+        s_cipher = (s[m] << (BIT - (i*4))) | s_cipher;
     }
-    return s_chiper;
+    return s_cipher;
 }
 
+uint16_t sbox_in(uint16_t c){
+	//解密s盒
+    uint16_t s_cipher = 0;
+    uint16_t mask = 0xf;
+    uint16_t m = 0;         //记录s盒函数的中间值
+    int i = 1;
+    for(i = 1;i <= (BIT/L);i++){
+        m = (c >> (BIT - (i*4))) & mask;
+        s_cipher = (s_in[m] << (BIT - (i*4))) | s_cipher;
+    }
+    return s_cipher;
+}
 
-spncode pbox(spncode c){
-    spncode v1, v2, v3, v4, v5, v6, v7;
+uint16_t pbox(uint16_t c){
+	//p盒置换
+    uint16_t v1, v2, v3, v4, v5, v6, v7;
     v1 = c & 0x8421;
     v2 = c & 0x4210;
     v3 = c & 0x0842;
@@ -46,14 +60,14 @@ spncode pbox(spncode c){
     return v1 | (v2 >> 3) | (v3 << 3) | (v4 >> 6) | (v5 << 6) | (v6 >> 9) | (v7 << 9);
 }
 
-spncode getk(int i){
-    //��������Կ
-    spncode k = K >> (BIT - (i*4));
+uint16_t getk(int i){
+    //获得轮密钥
+    uint16_t k = K >> (BIT - (i*4));
     return k;
 }
 
-spncode spn(spncode plain){
-    spncode middle = plain;    //middleΪ�м�ֵ
+uint16_t spn(uint16_t plain){
+    uint16_t middle = plain;    //middle为中间值
     int i = 0;
     for(i = 0;i < Nr-1 ;i++){
         middle = middle ^ getk(i);
@@ -65,11 +79,41 @@ spncode spn(spncode plain){
     return middle ^ getk(i);
 }
 
+uint16_t despn(uint16_t cipher){
+    uint16_t middle = cipher;
+    middle = middle ^ getk(Nr);
+    middle = sbox_in(middle);
+    int i;
+    for(i = Nr - 1; i > 0 ; i--){
+        middle = middle ^ getk(i);
+        middle = pbox(middle);
+        middle = sbox_in(middle);
+    }
+    return middle ^ getk(0);
+}
+
 int main(void){
-    spncode chiper = 0;         //����
-    spncode plain = 0;          //����
-    plain = 0x26b7;
-    chiper = spn(plain);
-    printf("Chiper is: %0x\n\n",chiper);
+    uint16_t cipher = 0x0000;         //密文
+    uint16_t plain = 0x26b7;          //默认明文
+    printf("Plain is: %0x\n\n",plain);
+    clock_t clockBegin1 = 0,clockBegin2 = 0,clockEnd = 0;
+
+    int i;
+    clockBegin1 = clock();
+    for(i = 0;i < 100000; i++){
+        cipher = spn(plain);
+    }
+    clockBegin2 = clock();
+    printf("After encryption,cipher is: %0x\n",cipher);
+    printf("Encryption time = (%0.2f/100000)ms\n", (double)(clockBegin2 - clockBegin1));
+
+    clockBegin2 = clock();
+    for(i = 0;i < 100000; i++){
+       plain = despn(cipher);
+    }
+    clockEnd = clock();
+    printf("\nAfter decryption,plain is: %0x\n",plain);
+    printf("Decryption time = (%0.2f/100000)ms\n", (double)(clockEnd - clockBegin2));
+
     return 0;
 }
